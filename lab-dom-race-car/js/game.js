03eb9f9cbc7ede216ef4fec259f41296ba357077
André Ferreira <andre.f.v.ferreira@gmail.com>
2024-04-13 16:59:37 +0100

    adds incomplete racecar

---
+++ lab-dom-race-car/js/game.js
@@ -0,0 +1,43 @@
+class Game {
+    constructor(){
+        this.startScreen = document.getElementById("game-intro")
+        this.gameScreen = document.getElementById("game-screen")
+        this.gameEndScreen = document.getElementById("game-end")
+        this.player = null;
+        this.heigth = 600;
+        this.width = 500;
+        this.obstacles = []
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+        this.gameIntervalId = null;
+        this.gameLoopFrequency = Math.floor(1000/60)
+    }
+
+    start(){
+        this.gameScreen.style.width = `${this.width}px`
+        this.gameScreen.style.height = `${this.heigth}px`
+
+        //removing the start screen from the page
+        this.startScreen.style.display = "none"
+        //show the gameScreen
+        this.gameScreen.style.display = "block"
+
+        //start the loop
+        this.gameIntervalId = setInterval(() => {
+            this.gameLoop()
+        }, this.gameLoopFrequency)
+    }
+
+    gameLoop(){
+        console.log("gameLoop")
+        if(this.gameIsOver){
+            clearInterval(this.gameIntervalId)
+        }
+    }
+
+    update(){
+
+    }
+
+}
---
+++ lab-dom-race-car/js/script.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+
+  let game = null;
+
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+
+  function startGame() {
+    console.log("start game");
+    game = new Game()
+
+    game.start()
+  
+  }
+};
